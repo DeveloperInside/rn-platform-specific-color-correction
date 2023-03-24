7b427dfc58ae04bf488a0d86caf7579c58545978
DeveloperInside <enesm.hancer@gmail.com>
2023-03-24 03:30:33 +0300

    export ColorGrid component

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,7 @@
-import * as React from 'react'
-import { Button, NativeModules, StyleSheet, Text, View } from 'react-native'
+import { NativeModules } from 'react-native'
 
-export const addOne = (input: number) => input + 1
+import ColorGrid from './components/ColorGrid'
 
-export const Counter = () => {
-  const [count, setCount] = React.useState(0)
-
-  return (
-    <View style={styles.container}>
-      <Text>You pressed {count} times</Text>
-      <Button onPress={() => setCount(addOne(count))} title='Press Me' />
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 200,
-  },
-})
+export { ColorGrid }
 
 export default NativeModules.PSColorCorrectionModule
