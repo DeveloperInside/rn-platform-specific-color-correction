3afedbde7adbc83e02f764d893f2626aca1d19e0
DeveloperInside <enesm.hancer@gmail.com>
2023-05-07 17:25:55 +0300

    chore: osColorBalance and other req funcs exported.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
 import { NativeModules } from 'react-native'
 
 import ColorGrid from './components/ColorGrid'
+import { convert } from './scripts/converters'
+import {
+  applyColorConversionMatrix,
+  shiftDisplayP3toSrgb,
+  shiftSrgbToDisplayP3,
+  sRgbTransferFunc,
+  sRgbTransferFuncReverse,
+  transformColor,
+} from './scripts/functions'
+import osColorBalance from './scripts/osColorBalance'
 
-export { ColorGrid }
+export {
+  applyColorConversionMatrix,
+  ColorGrid,
+  convert,
+  osColorBalance,
+  shiftDisplayP3toSrgb,
+  shiftSrgbToDisplayP3,
+  sRgbTransferFunc,
+  sRgbTransferFuncReverse,
+  transformColor,
+}
 
 export default NativeModules.PSColorCorrectionModule
